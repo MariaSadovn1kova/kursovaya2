d279c0f49e8e7b6e5cd1e1eca98bed22717fcef2
maria sadovnikova <msadovnikov904@gmail.com>
2021-12-13 19:12:53 +0800

    Добавьте файлы проекта.

---
+++ kursovaya2/Controllers/Request1Controller.cs
@@ -0,0 +1,23 @@
+﻿using kursovaya2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kursovaya2.Controllers
+{
+    public class Request1Controller : Controller
+    {
+        private UniversityEntities db = new UniversityEntities();
+        public ActionResult Index()
+        {
+            // Создаём параметр
+            SqlParameter parameter = new SqlParameter("@GroupNumber", 1000021);
+            // capital содержит результат выполнения хранимой процедуры
+            var registrations = db.Database.SqlQuery<Request1_Result>("Request1 @GroupNumber", parameter);
+            return View(registrations);
+        }
+    }
+}
---
+++ kursovaya2/Controllers/StudentController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using kursovaya2.Models;
+
+namespace kursovaya2.Controllers
+{
+    public class StudentController : Controller
+    {
+        private UniversityEntities db = new UniversityEntities();
+
+        // GET: Student
+        public ActionResult Index(int pg = 1)
+        {
+            List<Student> students = db.Student.ToList();
+            const int pageSize = 50;
+            if (pg < 1)
+                pg = 1;
+
+            int rescCount = students.Count();
+            var pages = new Pages(rescCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = students.Skip(recSkip).Take(pages.PageSize).ToList();
+            this.ViewBag.Pager = pages;
+
+            return View(data);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentNumber,Group_NumberOfGroup,Fulll_Name")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Student.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentNumber,Group_NumberOfGroup,Fulll_Name")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Student.Find(id);
+            db.Student.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
